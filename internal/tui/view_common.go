acbbd6dfe2345a57baf5b5ee14bd89e769093c59
Brandon Berhent <bbedward@gmail.com>
2025-03-31 17:36:59 -0400

    Truncate error message

--- internal/tui/view_common.go
+++ internal/tui/view_common.go
@@ -94,7 +94,13 @@ func viewError(m Model) string {
 		s.WriteString("\n")
 		s.WriteString(m.styles.Subtle.Render("Please check the logs for more details."))
 		s.WriteString("\n")
-		s.WriteString(m.styles.Error.Render(fmt.Sprintf("Error message %s", m.err.Error())))
+		errMsg := m.err.Error()
+		// truncate
+		lines := strings.Split(errMsg, "\n")
+		if len(lines) > 5 {
+			errMsg = "[...]\n" + strings.Join(lines[len(lines)-5:], "\n")
+		}
+		s.WriteString(m.styles.Error.Render(fmt.Sprintf("Error message %s", errMsg)))
 	} else if m.err != nil {
 		s.WriteString(m.styles.Error.Render(fmt.Sprintf("An error occurred: %v", m.err)))
 
