1582620d797509d3682cdb513f4569c223e20858
Brandon Berhent <bbedward@gmail.com>
2025-06-04 20:10:40 -0400

    Disable local storage also

--- internal/k3s/install.go
+++ internal/k3s/install.go
@@ -206,10 +206,9 @@ func (self *Installer) sendFact(fact string) {
 
 // Install sets up k3s and returns the kubeconfig path
 func (self *Installer) Install(ctx context.Context) (string, error) {
-	k3sInstallFlags := "--disable=traefik --kubelet-arg=fail-swap-on=false --cluster-init " +
+	k3sInstallFlags := "--disable=traefik --disable=local-storage --kubelet-arg=fail-swap-on=false --cluster-init " +
 		"--kubelet-arg=system-reserved=memory=512Mi,cpu=300m " +
 		"--kubelet-arg=kube-reserved=memory=256Mi,cpu=200m " +
-		"--kubelet-arg=enforce-node-allocatable=pods,system-reserved,kube-reserved " +
 		"--kubelet-arg=eviction-soft-grace-period=memory.available=30s " +
 		"--kubelet-arg=eviction-minimum-reclaim=memory.available=100Mi " +
 		"--kubelet-arg=eviction-max-pod-grace-period=30 " +
